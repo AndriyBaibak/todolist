d40b6419f589ba9126df26132f5ea14bda832ee5
Andriy Baibak <andriy.baibak@gmail.com>
2014-12-09 11:34:04 +0200

    delete CharacterEncoding from MainServlet

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -23,8 +23,7 @@ public class MainServlet extends HttpServlet {
     }
 
     public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
+      
         try {
             tasks = tasksDAO.getAllTasks();
         } catch (SQLException e) {
