c53278a0dabf7efc91ee1137e532a9981f893f0b
Andriy Baibak <andriy.baibak@gmail.com>
2015-02-02 08:38:04 +0200

    Rename some files

---
+++ src/main/java/ua/baibak/todolist/interfaces/TasksDao1.java
@@ -0,0 +1,12 @@
+package ua.baibak.todolist.interfaces;
+
+import java.util.Date;
+import java.util.List;
+
+public interface TasksDao1 {
+
+    public void save (String description, Date deadline) throws Exception;
+    public void updateTasks(String newData, String id, String type) throws Exception;
+    public List getAllTasks() throws Exception;
+    public void deleteTasks(int id) throws Exception;
+}
